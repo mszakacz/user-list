374cb1d0e5778e1c4b0aaecedd95455e24a5b529
Maciej <mszakacz@gmail.com>
2022-03-11 12:38:52 +0100

    refactor: repository is the app property

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:user_list/app/app.dart';
 import 'package:user_list/user_list/user_list.dart';
+import 'package:users_repository/users_repository.dart';
 
-class App extends StatefulWidget {
-  const App({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+    required this.usersRepository,
+  }) : super(key: key);
 
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  final AppRouter _appRouter = AppRouter();
+  final UsersRepository usersRepository;
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +25,7 @@ class _AppState extends State<App> {
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        onGenerateRoute: _appRouter.onGenerateRout,
+        home: const UsersPage(),
       ),
     );
   }
