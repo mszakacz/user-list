fe34a06a26128b40d44c291b40c4761386e1d57e
Maciej <mszakacz@gmail.com>
2022-03-12 19:37:54 +0100

    refactor(user_list): create UsersView as additional widget in UsersPage

--- lib/user_list/view/users_page.dart
+++ lib/user_list/view/users_page.dart
@@ -20,67 +20,78 @@ class UsersPage extends StatelessWidget {
     return BlocProvider(
       create: (context) => UsersBloc(
         usersRepository: RepositoryProvider.of<UsersRepository>(context),
-      )..add(UpdateUsersList()),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Users List'),
-          centerTitle: false,
-          backgroundColor: Colors.grey[800],
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(
-                Icons.settings,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                Navigator.of(context)
-                    .push(
-                      SettingsPage.route(),
-                    )
-                    .whenComplete(
-                      () => context.read<UsersBloc>().add(
-                            UpdateUsersList(),
-                          ),
-                    );
-              },
-            ),
-            IconButton(
-              icon: const Icon(
-                Icons.add_circle_sharp,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                Navigator.of(context)
-                    .push(
-                      NewUserPage.route(),
-                    )
-                    .whenComplete(
-                      () => context.read<UsersBloc>().add(
-                            UpdateUsersList(),
-                          ),
-                    );
-              },
-            ),
-            IconButton(
-              icon: const Icon(
-                Icons.refresh_rounded,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                context.read<UsersBloc>().add(
-                      UpdateUsersList(),
-                    );
-              },
-            )
-          ],
+      )..add(
+          UpdateUsersList(),
         ),
-        body: BlocBuilder<UsersBloc, UsersState>(
-          builder: (context, state) {
-            return const Center(
-              child: UsersListWidget(),
-            );
-          },
-        ),
+      child: const UsersView(),
+    );
+  }
+}
+
+class UsersView extends StatelessWidget {
+  const UsersView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Users List'),
+        centerTitle: false,
+        backgroundColor: Colors.grey[800],
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.of(context)
+                  .push(
+                    SettingsPage.route(),
+                  )
+                  .whenComplete(
+                    () => context.read<UsersBloc>().add(
+                          UpdateUsersList(),
+                        ),
+                  );
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.add_circle_sharp,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.of(context)
+                  .push(
+                    NewUserPage.route(),
+                  )
+                  .whenComplete(
+                    () => context.read<UsersBloc>().add(
+                          UpdateUsersList(),
+                        ),
+                  );
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.refresh_rounded,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              context.read<UsersBloc>().add(
+                    UpdateUsersList(),
+                  );
+            },
+          )
+        ],
+      ),
+      body: BlocBuilder<UsersBloc, UsersState>(
+        builder: (context, state) {
+          return const Center(
+            child: UsersListWidget(),
+          );
+        },
       ),
     );
   }
