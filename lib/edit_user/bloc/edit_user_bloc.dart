409464c735aee208dac88e562e46d313d213a13c
Maciej <mszakacz@gmail.com>
2022-03-18 04:01:13 +0100

    refactor(edit_user): rename EditUsersState, add success and failureToGet status

--- lib/edit_user/bloc/edit_user_bloc.dart
+++ lib/edit_user/bloc/edit_user_bloc.dart
@@ -6,9 +6,10 @@ import 'package:users_repository/users_repository.dart';
 part 'edit_user_event.dart';
 part 'edit_user_state.dart';
 
-class EditUserBloc extends Bloc<EditUserEvent, UserState> {
+class EditUserBloc extends Bloc<EditUserEvent, EditUserState> {
   EditUserBloc({required this.usersRepository})
-      : super(const UserState(status: EditUserStatus.loading, user: User())) {
+      : super(
+            const EditUserState(status: EditUserStatus.loading, user: User())) {
     on<SetUserByID>(_onSetUserByID);
     on<NameEditing>(_onNameEditing);
     on<LastnameEditing>(_onLastnameEditing);
@@ -18,28 +19,33 @@ class EditUserBloc extends Bloc<EditUserEvent, UserState> {
   final UsersRepository usersRepository;
 
   Future<void> _onSetUserByID(
-      SetUserByID event, Emitter<UserState> emit) async {
+      SetUserByID event, Emitter<EditUserState> emit) async {
     emit(state.copyWith(status: EditUserStatus.loading));
     try {
       final user = await usersRepository.getUser(event.id);
       emit(state.copyWith(user: user, status: EditUserStatus.edit));
     } catch (_) {
-      emit(state.copyWith(status: EditUserStatus.failure));
+      emit(state.copyWith(status: EditUserStatus.failureToGet));
     }
   }
 
-  void _onNameEditing(NameEditing event, Emitter<UserState> emit) {
+  void _onNameEditing(NameEditing event, Emitter<EditUserState> emit) {
     emit(state.copyWith(user: state.user.copyWith(firstname: event.firstname)));
   }
 
-  void _onLastnameEditing(LastnameEditing event, Emitter<UserState> emit) {
+  void _onLastnameEditing(LastnameEditing event, Emitter<EditUserState> emit) {
     emit(state.copyWith(user: state.user.copyWith(lastname: event.lastname)));
   }
 
-  Future<void> _onSaveUser(SaveUser event, Emitter<UserState> emit) async {
+  Future<void> _onSaveUser(SaveUser event, Emitter<EditUserState> emit) async {
     emit(state.copyWith(status: EditUserStatus.posting));
     try {
       await usersRepository.updateUser(state.user);
+      emit(
+        state.copyWith(
+          status: EditUserStatus.success,
+        ),
+      );
     } catch (_) {
       emit(state.copyWith(status: EditUserStatus.failure));
     }
