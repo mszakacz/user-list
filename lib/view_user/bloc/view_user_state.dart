84e60e3bda9dcdf85a2988821d08a97d94281a8f
Maciej <mszakacz@gmail.com>
2022-03-18 04:26:23 +0100

    feat(view_user): add ViewUserMessage as a ViewUserState property

--- lib/view_user/bloc/view_user_state.dart
+++ lib/view_user/bloc/view_user_state.dart
@@ -1,31 +1,45 @@
 part of 'view_user_bloc.dart';
 
-enum UserActivationStatus {
+enum ViewUserStatus {
   loading,
   active,
   locked,
-  failure,
+  deleted,
+}
+
+enum ViewUserMessage {
+  activated,
+  locked,
+  failureToActivate,
+  failureToLock,
+  failureToDelete,
+  failureToGet,
+  empty,
   deleted,
 }
 
 class ViewUserState extends Equatable {
-  final UserActivationStatus status;
-  final User user;
   const ViewUserState({
-    this.status = UserActivationStatus.loading,
+    this.status = ViewUserStatus.loading,
     this.user = const User(),
+    this.message = ViewUserMessage.empty,
   });
+  final ViewUserStatus status;
+  final User user;
+  final ViewUserMessage message;
 
   @override
-  List<Object> get props => [status, user];
+  List<Object> get props => [status, user, message];
 
   ViewUserState copyWith({
+    ViewUserStatus? status,
     User? user,
-    UserActivationStatus? status,
+    ViewUserMessage? message,
   }) {
     return ViewUserState(
-      user: user ?? this.user,
       status: status ?? this.status,
+      user: user ?? this.user,
+      message: message ?? this.message,
     );
   }
 }
